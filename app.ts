5a00983b3ba7848d685d4f589df6d54b69dbdbe3
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-18 22:07:18 -0600

    added types to object

--- app.ts
+++ app.ts
@@ -1,13 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  if (showResult) {
-    console.log(phrase + n1 + n2);
-  } else {
-    return n1 + n2;
-  }
-}
-const num1 = "5";
-const num2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Mitchel",
+  age: 35,
+};
 
-add(+num1, +num2, printResult, resultPhrase);
+console.log(person.name);
