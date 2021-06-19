7efc0795a44c949b7f08c2b6d3f70cee92824a86
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-18 21:50:07 -0600

    added more types

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(phrase + n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 const num1 = "5";
 const num2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(+num1, +num2);
-console.log(result);
+add(+num1, +num2, printResult, resultPhrase);
