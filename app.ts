bc482e4388a206c1f392b43a7789103adfaef9f3
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-18 23:06:25 -0600

    added void type

--- app.ts
+++ app.ts
@@ -1,30 +1,8 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text" //literal type
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === "as-number") {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult(num: number): void {
+  console.log("Result: ", num);
+}
+printResult(add(5, 12));
